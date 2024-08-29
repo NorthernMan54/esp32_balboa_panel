549728e3b2ac9883bf24753902beafed63899002
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2024-08-29 08:59:37 -0400

    Working

--- scripts/extra_script.py
+++ scripts/extra_script.py
@@ -4,18 +4,24 @@ import os
 # Add the following line to your platformio.ini to enable this script
 # extra_scripts = post:scripts/extra_script.py
 
+# VITE_LOCAL_HOSTING=Enabled
+#
+# or
+#
+# VITE_HOSTING_URL=http://spaPanel-3C71BF9DFA90.local.
 
 def before_buildfs(source, target, env):
     if not os.path.exists('balboa-spa'):
         print('Cloning balboa-spa')
         os.system('git clone https://github.com/NorthernMan54/balboa-spa.git')
         os.chdir('balboa-spa')
+        print("Option A: VITE_LOCAL_HOSTING=Enabled")
+        print("Option B: VITE_HOSTING_URL=http://" + env['UPLOAD_PORT'])
 
         with open('.env', 'w') as file:
-            file.write("VITE_URL=http://" + env['UPLOAD_PORT'])
+            file.write("VITE_LOCAL_HOSTING=Enabled")
         os.system('npm install')
         os.system('npm run build')
         os.chdir('../')
-
 
 env.AddPreAction('$BUILD_DIR/littlefs.bin', before_buildfs)
